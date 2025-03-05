9dd4e7fcf77f9188071003382e483e8acb0c1cb3
kaleabmarkos <kaleabgessese@gmail.com>
2025-03-05 12:08:55 -0600

    Included comments.

--- int.py
+++ int.py
@@ -1,4 +1,4 @@
-INTEGER, PLUS, EOF, SUB, MUL, DIV = 'INTEGER', 'PLUS', 'EOF', 'SUB','MUL', 'DIV'
+INTEGER, PLUS, EOF, SUB, MUL, DIV, LBRACK, RBRACK = 'INTEGER', 'PLUS', 'EOF', 'SUB','MUL', 'DIV', '(', ')'
 '''
 EOF = end of file
 '''
@@ -9,6 +9,13 @@ class Token:
         self.value = value
 
     def __str__(self):
+        """String representation of the class instance.
+
+        Examples:
+            Token(INTEGER, 3)
+            Token(PLUS, '+')
+            Token(MUL, '*')
+        """
         return f"{self.token_type} with value of {self.value}"
     
 class Lexer:
@@ -21,6 +28,7 @@ class Lexer:
         raise Exception("Invalid character")
     
     def advance(self):
+        """Advance the `pos` pointer and set the `current_char` variable."""
         self.pos+=1
         if self.pos >= len(self.text):
             self.current_char = None
@@ -32,6 +40,7 @@ class Lexer:
             self.advance()
     
     def integer(self):
+        """ Gets the integers in one term(also if multi digit and returns it"""
         result = ''
         while self.current_char is not None and self.current_char.isdigit():
             result+=self.current_char
@@ -39,6 +48,7 @@ class Lexer:
         return int(result)
     
     def get_next_token(self):
+        """ Tokenizer, responsible for breaking down the whole string into tokens, one at a time"""
         while self.current_char is not None:
             if self.current_char.isspace():
                 self.skip_white_space()
@@ -58,6 +68,13 @@ class Lexer:
             if self.current_char == '/':
                 self.advance()
                 return Token(DIV,'/')
+            if self.current_char == '(':
+                self.advance()
+                return Token(LBRACK,')')
+            if self.current_char == ')':
+                self.advance()
+                return Token(RBRACK,')')
+            
             
             self.Error()
         return Token(EOF, None)
@@ -73,8 +90,12 @@ class Interpreter:
         raise Exception("Error parsing input")
 
 
-
     def eat(self, token_type):
+        """ compare the current token type with the passed token
+         type and if they match then "eat" the current token
+         and assign the next token to the self.current_token,
+         otherwise raise an exception(calls error)."""
+        
         if self.cur_token.token_type == token_type:
             self.cur_token = self.lexer.get_next_token()
         else:
@@ -82,8 +103,14 @@ class Interpreter:
     
     def factor(self):
         token = self.cur_token
-        self.eat(INTEGER)
-        return token.value
+        if token.token_type == INTEGER:
+            self.eat(INTEGER)
+            return token.value
+        elif token.token_type == LBRACK:
+            self.eat(LBRACK)
+            result = self.expr()
+            self.eat(RBRACK)
+            return result
 
     def terms(self):
         result = self.factor()
@@ -101,6 +128,16 @@ class Interpreter:
 
     
     def expr(self):
+        """
+        Arithmetic expression parser / interpreter.
+
+        calc> 7 + 3 * (10 / (12 / (3 + 1) - 1))
+        22
+
+        expr   : term ((PLUS | MINUS) term)*
+        term   : factor ((MUL | DIV) factor)*
+        factor : INTEGER | LPAREN expr RPAREN
+        """
         result = self.terms()
 
         while self.cur_token.token_type in [PLUS, SUB]:
@@ -129,4 +166,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
